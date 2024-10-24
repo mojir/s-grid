2a0663648003b701843d4c3b73387e661f8ba657
Albert Mojir <albert.mojir@youcruit.com>
2024-10-24 22:15:10 +0200

    Styling a basic sheet

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,7 +1,148 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, useTemplateRef } from 'vue'
+
+const rows = ref(
+  Array.from({ length: 99 }, (_, i) => ({
+    id: `${i + 1}`,
+    height: 30,
+  })),
+)
+
+const cols = ref(
+  Array.from({ length: 26 * 3 }, (_, i) => ({
+    id: getColHeader(i),
+    width: 100,
+  })),
+)
+
+function getColHeader(col: number) {
+  let result = ''
+  while (col >= 0) {
+    result = String.fromCharCode((col % 26) + 65) + result
+    col = Math.floor(col / 26) - 1
+  }
+  return result
+}
+
+const rowHeaderWidth = 50
+const colHeaderHeight = 25
+function dim(w: number | null, h: number | null) {
+  const result: Record<string, string> = {}
+  if (w !== null) {
+    result.width = `${w}px`
+    result.minWidth = `${w}px`
+  }
+  if (h !== null) {
+    result.height = `${h}px`
+    result.minHeight = `${h}px`
+  }
+  return result
+}
+
+const sheetRef = useTemplateRef('sheet')
+const rowHeaderRef = useTemplateRef('rowHeader')
+const colHeaderRef = useTemplateRef('colHeader')
+let activeScrollElement: HTMLElement | null = null
+let scrollTimer = 0
+
+function syncScroll(event: Event) {
+  // Avoid looping when another div trigger scroll event
+  if (activeScrollElement && activeScrollElement !== event.target) {
+    return
+  }
+
+  activeScrollElement = event.target as HTMLElement
+
+  if (activeScrollElement === sheetRef.value) {
+    rowHeaderRef.value!.scrollTop = activeScrollElement.scrollTop
+    colHeaderRef.value!.scrollLeft = activeScrollElement.scrollLeft
+  } else if (activeScrollElement === rowHeaderRef.value) {
+    sheetRef.value!.scrollTop = activeScrollElement.scrollTop
+  } else if (activeScrollElement === colHeaderRef.value) {
+    sheetRef.value!.scrollLeft = activeScrollElement.scrollLeft
+  }
+
+  // Reset scrollingDiv after sync to allow future scroll events
+  clearTimeout(scrollTimer)
+  scrollTimer = setTimeout(() => {
+    activeScrollElement = null
+  }, 20)
+}
+</script>
 
 <template>
-  <main>
-    <h1>Home</h1>
+  <main class="flex flex-col h-screen bg-slate-900 text-slate-300">
+    <div
+      :style="dim(null, 50)"
+      class="flex justify-center items-center text-2xl"
+    >
+      Litsheet
+    </div>
+
+    <div class="flex flex-grow flex-col overflow-hidden">
+      <div
+        class="flex"
+        :style="dim(null, colHeaderHeight)"
+      >
+        <div
+          class="flex bg-slate-800 box-border border-b border-r border-slate-700"
+          :style="dim(rowHeaderWidth, colHeaderHeight)"
+        ></div>
+        <div
+          class="flex overflow-y-auto bg-slate-800 no-scrollbar"
+          ref="colHeader"
+          @scroll="syncScroll"
+        >
+          <div
+            v-for="col of cols"
+            :key="col.id"
+            :style="dim(col.width, colHeaderHeight)"
+            class="flex overflow-hidden justify-center text-xs items-center box-border border-r border-slate-700"
+          >
+            {{ col.id }}
+          </div>
+        </div>
+      </div>
+      <div class="flex overflow-hidden">
+        <div
+          ref="rowHeader"
+          class="flex flex-col overflow-x-auto bg-slate-800 no-scrollbar"
+          :style="dim(rowHeaderWidth, null)"
+          @scroll="syncScroll"
+        >
+          <div
+            v-for="row of rows"
+            :key="row.id"
+            :style="dim(rowHeaderWidth, row.height)"
+            class="flex overflow-hidden justify-center items-center text-xs box-border border-b border-slate-700"
+          >
+            {{ row.id }}
+          </div>
+        </div>
+        <div
+          class="overflow-auto"
+          ref="sheet"
+          @scroll="syncScroll"
+        >
+          <div
+            v-for="row of rows"
+            :key="row.id"
+            :style="dim(null, row.height)"
+            class="flex"
+          >
+            <div
+              v-for="col of cols"
+              :key="col.id"
+              :style="dim(col.width, row.height)"
+              class="flex overflow-hidden box-border border-r border-b border-slate-800"
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      :style="dim(null, 30)"
+      class="bg-slate-800 box-border border-t border-slate-700"
+    ></div>
   </main>
 </template>
