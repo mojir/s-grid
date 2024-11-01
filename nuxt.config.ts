a48657796b3b198e5c6a09fc354f9eb83b12b270
Albert Mojir <albert.mojir@youcruit.com>
2024-11-01 12:46:54 +0100

    Changing colormode to system

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
   },
   colorMode: {
     classSuffix: '',
-    preference: 'dark',
+    preference: 'system',
     fallback: 'dark',
   },
   compatibilityDate: '2024-04-03',
