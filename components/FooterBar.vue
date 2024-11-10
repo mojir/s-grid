53a2a2817b9cea978d19fed0e5155b89ba83f69d
Albert Mojir <albert.mojir@youcruit.com>
2024-11-10 11:15:36 +0100

    Refactor FooterBar component to improve layout and error message display

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -2,9 +2,10 @@
 import { isLitsError } from '@mojir/lits'
 import { computed } from 'vue'
 import { useGrid } from '@/composables/useGrid'
-import { hs } from '~/lib/utils'
 
 const { grid } = useGrid()
+const { rowHeaderWidth } = useRowsAndCols()
+
 const errorMessage = computed(() => {
   const cell = grid.value.getCurrentCell()
   const output = cell?.output.value
@@ -27,16 +28,16 @@ const errorMessage = computed(() => {
 
 <template>
   <div
-    :style="hs(30)"
-    class="flex dark:bg-slate-800 bg-gray-200 box-border border-t dark:border-slate-700 border-gray-300 items-center px-2 text-sm justify-between"
+    :style="{
+      paddingLeft: `${rowHeaderWidth + 5}px`,
+    }"
+    class="truncate flex h-12 dark:bg-slate-800 bg-gray-200 box-border border-t dark:border-slate-700 border-gray-300 items-center px-2 text-sm justify-between"
   >
-    <div>
-      <div
-        v-if="errorMessage"
-        class="flex-1 text-red-500"
-      >
-        Error: {{ errorMessage }}
-      </div>
+    <div
+      v-if="errorMessage"
+      class="flex-1 text-red-500"
+    >
+      {{ errorMessage }}
     </div>
   </div>
 </template>
