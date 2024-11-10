1dc3742b1efacd9b5f5e5c0c4af8a4be4bb71353
Albert Mojir <albert.mojir@youcruit.com>
2024-11-10 11:56:25 +0100

    Refactor FooterBar component to enhance error message display and improve layout styling

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -4,14 +4,13 @@ import { computed } from 'vue'
 import { useGrid } from '@/composables/useGrid'
 
 const { grid } = useGrid()
-const { rowHeaderWidth } = useRowsAndCols()
 
 const errorMessage = computed(() => {
   const cell = grid.value.getCurrentCell()
   const output = cell?.output.value
   if (output instanceof Error) {
     if (isLitsError(output)) {
-      return output.shortMessage
+      return output.message
     }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const message = (output as any)?.message
@@ -28,14 +27,11 @@ const errorMessage = computed(() => {
 
 <template>
   <div
-    :style="{
-      paddingLeft: `${rowHeaderWidth + 5}px`,
-    }"
-    class="truncate flex h-12 dark:bg-slate-800 bg-gray-200 box-border border-t dark:border-slate-700 border-gray-300 items-center px-2 text-sm justify-between"
+    class="pl-4 flex h-auto items-center text-sm justify-between dark:bg-slate-900 bg-white box-border border-t dark:border-slate-700 border-gray-300"
   >
     <div
       v-if="errorMessage"
-      class="flex-1 text-red-500"
+      class="whitespace-pre-wrap flex-1 text-red-500 py-2 max-h-40 overflow-auto"
     >
       {{ errorMessage }}
     </div>
