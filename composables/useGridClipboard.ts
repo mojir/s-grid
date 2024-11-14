8661d085e0670a439f6acdc9a535d91a100b30d4
Albert Mojir <albert.mojir@youcruit.com>
2024-11-14 22:39:14 +0100

    Refactor clipboard handling to paste in selection

--- composables/useGridClipboard.ts
+++ composables/useGridClipboard.ts
@@ -1,11 +1,11 @@
 import type { CellJson } from '~/lib/Cell'
 import { CellId } from '~/lib/CellId'
-import { CellRange } from '~/lib/CellRange'
+import type { CellRange } from '~/lib/CellRange'
 import { matrixForEach, matrixMap } from '~/lib/matrix'
 import { transformGridReference } from '~/lib/transformFormula'
 
 type Clipboard = {
-  rangeId: string
+  range: CellRange
   cells: CellJson[][]
 }
 
@@ -23,7 +23,7 @@ export default function useGridClipboard() {
     }
 
     clipboard.value = {
-      rangeId: selection.value.id,
+      range: selection.value,
       cells: matrixMap(selection.value.getCellIdMatrix(), cellId => grid.value.getCell(cellId).getJson()),
     }
   }
@@ -34,26 +34,61 @@ export default function useGridClipboard() {
     cutCellIds.value = selection.value.getAllCellIds()
   }
 
+  function getPastePositions(): CellId[] {
+    if (!clipboard.value) {
+      return []
+    }
+
+    if (cut.value) {
+      return [selection.value.start]
+    }
+
+    const selectionWidth = selection.value.end.colIndex - selection.value.start.colIndex + 1
+    const selectionHeight = selection.value.end.rowIndex - selection.value.start.rowIndex + 1
+
+    const range = clipboard.value.range
+    const rangeWidth = range.end.colIndex - range.start.colIndex + 1
+    const rangeHeight = range.end.rowIndex - range.start.rowIndex + 1
+
+    const result: CellId[] = []
+    const startRow = selection.value.start.rowIndex
+    const startCol = selection.value.start.colIndex
+    let row = startRow
+    let col = startCol
+    // Populate result arrays with the positions to paste the clipboard
+    do {
+      do {
+        result.push(CellId.fromCoords(row, col))
+        col += rangeWidth
+      } while (col - startCol + rangeWidth <= selectionWidth)
+      row += rangeHeight
+      col = startCol
+    } while (row - startRow + rangeHeight <= selectionHeight)
+
+    return result
+  }
+
   function pasteSelection() {
     if (!clipboard.value) {
       return
     }
+    const clipboardCells = clipboard.value.cells
+    const fromPosition = clipboard.value.range.start
+    getPastePositions().forEach((toPosition) => {
+      const movement = fromPosition.getMovementTo(toPosition)
 
-    const range = CellRange.fromId(clipboard.value.rangeId)
-    const fromPosition = range.start
-    const toPosition = selection.value.start
-    const movement = fromPosition.getMovementTo(toPosition)
-
-    matrixForEach(clipboard.value.cells, (cellJson, [rowIndex, colIndex]) => {
-      const cellId = CellId.fromCoords(toPosition.rowIndex + rowIndex, toPosition.colIndex + colIndex)
-      const cell = grid.value.getCell(cellId)
-      let input = cellJson.input
-      if (input.startsWith('=')) {
-        input = `=${transformGridReference(cellJson.input.slice(1), { type: 'move', movement })}`
-      }
-      cell.setJson({ ...cellJson, input })
+      matrixForEach(clipboardCells, (cellJson, [rowIndex, colIndex]) => {
+        const cellId = CellId.fromCoords(toPosition.rowIndex + rowIndex, toPosition.colIndex + colIndex)
+        const cell = grid.value.getCell(cellId)
+        let input = cellJson.input
+        if (input.startsWith('=')) {
+          input = `=${transformGridReference(cellJson.input.slice(1), { type: 'move', movement })}`
+        }
+        cell.setJson({ ...cellJson, input })
+      })
     })
 
+    // TODO, this logic is not ready yet
     if (cut.value) {
       cutCellIds.value.forEach((cellId) => {
         grid.value.clear(cellId)
